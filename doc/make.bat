2c95d57834fba18db505784de20395a2adb589f9
Joao Vitor Carli Pereira <jvitor.carli@gmail.com>
2024-03-27 12:57:04 -0300

    docs: add sphinx-autobuild dependency for developing the documentation

--- doc/make.bat
+++ doc/make.bat
@@ -25,11 +25,19 @@ if errorlevel 9009 (
 
 if "%1" == "" goto help
 
+REM Check for livehtml target
+if "%1" == "livehtml" goto livehtml
+
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
 
 :help
 %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+goto end
+
+:livehtml
+sphinx-autobuild "%SOURCEDIR%" "%BUILDDIR%" %SPHINXOPTS% %O%
+goto end
 
 :end
 popd
