088550b48456eb01f17087e8486ff8af23ce47ed
Joao Vitor Carli Pereira <jvitor.carli@gmail.com>
2024-03-29 17:36:53 -0300

    refactor!: follow PEP 8 and make modules names all-lowercase

--- src/femder/__init__.py
+++ src/femder/__init__.py
@@ -6,15 +6,15 @@ Created on Sat Nov 28 00:24:04 2020
 """
 
 from femder.controlsair import AirProperties, AlgControls, sph2cart, cart2sph
-from femder.FEM_1D import FEM1D
+from femder.fem_1d import FEM1D
 from femder.grid_importing import GridImport, GridImport3D
-from femder.FEM_3D import FEM3D, fem_load, p2SPL
-from femder.BoundaryConditions import BC
+from femder.fem_3d import FEM3D, fem_load, p2SPL
+from femder.boundary_conditions import BC
 
 # from femder.TMM_rina_improved import TMM
 from femder.sources import Source
 from femder.receivers import Receiver
-from femder.GeometryGenerate import GeometryGenerator
+from femder.geometry_generate import GeometryGenerator
 from femder.optimization_helpers import (
     r_s_for_room,
     r_s_positions,
@@ -22,7 +22,7 @@ from femder.optimization_helpers import (
     fitness_metric,
 )
 from femder.utils import IR, SBIR
-from femder.BEM_3D import BEM3D
+from femder.bem_3d import BEM3D
 from tmma.tmm import TMM
 from femder import plot_tools
 from femder import fem_numerical
